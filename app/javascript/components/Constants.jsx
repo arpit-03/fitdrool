5b1ed5adaa34c6985ba1aa98697caf7325731270
arpit-03 <arpitdeveloper03@gmail.com>
2020-08-01 16:35:47 +0530

    finalv0.14

--- app/javascript/components/Constants.jsx
+++ app/javascript/components/Constants.jsx
@@ -1,4 +1,4 @@
 export default {
-  backpath: "http://192.168.1.6:8081",
-  videopath: "http://localhost:8082",
+  backpath: "http://45.130.229.149:8081",
+  videopath: "http://45.130.229.149:8082",
 };
